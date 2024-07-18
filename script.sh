a49a8dec0207041b9c89cf97e34d3ed963db2eb1
viruskizz <kizzaraiva@gmail.com>
2024-07-18 21:27:05 +0700

    fix: version syntax

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws deploy create-deployment \
-    --application-name Cloud1-codedeploy-app \
-    --deployment-group-name cloud1-group \
-    --github-location repository=viruskizz/42bangkok-cloud-1,commitId=7d714da5a6d658cf0294101b0fb66543654a3cbc \
-    --region ap-southeast-1
+  --application-name Cloud1-codedeploy-app \
+  --deployment-group-name cloud1-group \
+  --github-location repository=viruskizz/42bangkok-cloud-1,commitId=1e442f7c16f5f0cc5a6a70796679ec7e9506fb31 \
+  --region ap-southeast-1
