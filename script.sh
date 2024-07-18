f9530e642d720af730fbc4be090007c85cc87b73
viruskizz <kizzaraiva@gmail.com>
2024-07-18 21:34:20 +0700

    fix chmod

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 aws deploy create-deployment \
   --application-name Cloud1-codedeploy-app \
   --deployment-group-name cloud1-group \
-  --github-location repository=viruskizz/42bangkok-cloud-1,commitId=1e442f7c16f5f0cc5a6a70796679ec7e9506fb31 \
+  --github-location repository=viruskizz/42bangkok-cloud-1,commitId=a49a8dec0207041b9c89cf97e34d3ed963db2eb1 \
   --region ap-southeast-1
