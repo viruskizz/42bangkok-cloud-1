3f0b14f5eee33210a3f9b0631d5d305981150894
viruskizz <kizzaraiva@gmail.com>
2024-07-19 02:08:32 +0700

    feat: back to simple

--- docker/src/requirements/wordpress/tools/adminer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ADMIN_PATH="/var/www/html/wordpress/adminer"
-mkdir -p $ADMIN_PATH
-wget "http://www.adminer.org/latest.php" -O $ADMIN_PATH/index.php
-chown -R www-data:www-data $ADMIN_PATH/index.php
-chmod 755 $ADMIN_PATH/index.php
