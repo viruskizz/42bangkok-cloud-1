d3a8a2aed0fe01c2283976df61a0d2836c05c05a
viruskizz <kizzaraiva@gmail.com>
2024-07-18 21:47:20 +0700

    fix: auto

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-COMMIT_ID=f9530e642d720af730fbc4be090007c85cc87b73
+COMMIT_ID=3b3269f0de29b4b035154b187526337b55d36d77
+REPOSITORY=viruskizz/42bangkok-cloud-1
 
 aws deploy create-deployment \
   --application-name Cloud1-codedeploy-app \
   --deployment-group-name cloud1-group \
-  --github-location repository=viruskizz/42bangkok-cloud-1,commitId=$COMMIT_ID \
+  --github-location repository=$REPOSITORY,commitId=$COMMIT_ID \
   --region ap-southeast-1
