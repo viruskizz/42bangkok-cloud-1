3b1b6a45d5a14142a8867da264b4157c6d77b1f3
viruskizz <kizzaraiva@gmail.com>
2024-07-19 14:20:49 +0700

    fix script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-COMMIT_ID=d3a8a2aed0fe01c2283976df61a0d2836c05c05a
+COMMIT_ID=979603be80656b4738406372824eee75970fbf5b
 REPOSITORY=viruskizz/42bangkok-cloud-1
 
 aws deploy create-deployment \
   --application-name Cloud1-codedeploy-app \
-  --deployment-group-name cloud1-group \
+  --deployment-group-name Cloud1-group \
   --github-location repository=$REPOSITORY,commitId=$COMMIT_ID \
   --region ap-southeast-1
--- scripts/down-docker.sh
+++ scripts/down-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd down-docker.sh
+cd srcs
 docker compose down
