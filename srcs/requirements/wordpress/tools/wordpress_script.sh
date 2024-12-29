2c6a23e5cbe4d709cdc8754ae99679d9b2be8d80
shivalux <shivarakii@Shivaluxs-MacBook-Pro.local>
2024-12-29 08:09:28 +0700

    wordpress::theme-activate

--- srcs/requirements/wordpress/tools/wordpress_script.sh
+++ srcs/requirements/wordpress/tools/wordpress_script.sh
@@ -10,4 +10,5 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	# * create config file for wordpress to passing thought wordpress website ;
 	wp-cli config create --dbname=$MYSQL_NAME --dbuser=$MYSQL_USERNAME_ADMIN --dbpass=$MYSQL_PASSWORD_ADMIN --dbhost=$MYSQL_HOST --allow-root;
 	wp-cli core install --url=$DOMAIN_NAME --title=$WEB_TITLE --admin_user=$MYSQL_USERNAME_ADMIN --admin_password=$MYSQL_PASSWORD_ADMIN --admin_email=$WEB_EMAIL --allow-root;
+	wp-cli theme install twentytwentyfour --activate --allow-root
 fi
